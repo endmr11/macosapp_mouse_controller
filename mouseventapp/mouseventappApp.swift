8e5b16c6bc55e9fde9dbaa4835f6dd48850e100e
eren <erndemir.1@gmail.com>
2024-03-09 16:52:14 +0300

    Initial Commit

---
+++ mouseventapp/mouseventappApp.swift
@@ -0,0 +1,17 @@
+//
+//  mouseventappApp.swift
+//  mouseventapp
+//
+//  Created by Eren Demir on 9.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct mouseventappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
